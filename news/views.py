4e92a2c1105c73aa6decb02e8089ae26cc6eeb4d
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-03-02 02:09:50 +0600

    views for api request

--- news/views.py
+++ news/views.py
@@ -1,3 +1,24 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+PROTHOM_ALO_API = "https://en.prothomalo.com/api/v1/collections/cricket-sports?offset=20&limit=100"
+
+def fetch_news(request):
+    response = requests.get(PROTHOM_ALO_API)
+    if response.status_code == 200:
+        data = response.json()
+        news_items = data.get("items", [])  # Get the items list
+        news_list = []
+
+        for item in news_items:
+            story = item.get("story", {})
+            news_list.append({
+                "title": story.get("seo", {}).get("meta-title", "No Title"),
+                "description": story.get("seo", {}).get("meta-description", "No Description"),
+                "author": story.get("author-name", "Unknown"),
+            })
+
+    else:
+        news_list = []
+
+    return render(request, 'news/home.html', {"news": news_list})
